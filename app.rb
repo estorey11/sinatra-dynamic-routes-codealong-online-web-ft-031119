07c2453602fd45b7937241cd41187d574431334f
Eric Storey <estorey@gmail.com>
2019-04-25 20:43:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do
-    "#{params[:num1] * params[:num2]}"
+    @total=params[:num1] * params[:num2]
+    "#{@total}"
   end
 
 end
